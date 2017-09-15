45f95889d3de9d6b7460f34ab08a09cc2aacf0de
ngreen64 <ng.x64.me@gmail.com>
2017-09-15 15:22:14 +0100

    Update create_eggs.py

--- create_eggs.py
+++ create_eggs.py
@@ -4,18 +4,16 @@ import math
 import sys
 import uuid
 
-if len(sys.argv) < 1:
-    print("No arguments passed to script, generating infinite eggs between 1 and 100,000,000")
+if len(sys.argv) < 2:
+    print("No arguments passed to script, generating infinite eggs between 1 and 100,000,000,000")
     lifetime = -1
-    upperlimit = 100000000
+    upperlimit = 10000000000
 else:
     lifetime = int(sys.argv[1])
     upperlimit = int(sys.argv[2])
     
-eggname = uuid.uuid4()
-
 def name_egg():
-    eggname
+    return uuid.uuid4()
 
 def get_random():
     return random.randint(2,upperlimit)
@@ -33,5 +31,5 @@ while lifetime >= 1 or lifetime < 0:
          X = X + 1
  if prime == "yes":
     print(str(number))
-    print(name_egg)
+    print(name_egg())
     lifetime -= 1
