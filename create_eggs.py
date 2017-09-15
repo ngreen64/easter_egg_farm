95a9f0864b75adfdf293a357ce7b64a16cb3245b
ngreen64 <ng.x64.me@gmail.com>
2017-09-15 15:10:53 +0100

    Update create_eggs.py

--- create_eggs.py
+++ create_eggs.py
@@ -5,15 +5,23 @@ import sys
 import uuid
 
 if len(sys.argv) < 1:
-    print("No arguments passed to script")
-    sys.exit()
+    print("No arguments passed to script, generating infinite eggs between 1 and 100,000,000")
+#    sys.exit()
 
-if len(sys.argv) < 2:
-    print("Usage: script.py <lifetime> <upperlimitforprime>")
-    sys.exit()
+#if len(sys.argv) < 2:
+#   print("Usage: script.py <lifetime> <upperlimitforprime>")
+#   sys.exit()
 
-lifetime = int(sys.argv[1])
-upperlimit = int(sys.argv[2])
+if sys.argv[1]:
+    lifetime = int(sys.argv[1])
+else:
+    lifetime = -1
+    
+if sys.argv[2]:
+    upperlimit = int(sys.argv[2])
+else:
+    upperlimit = 100000000
+    
 eggname = uuid.uuid4()
 
 def name_egg():
@@ -22,7 +30,7 @@ def name_egg():
 def get_random():
     return random.randint(2,upperlimit)
 
-while lifetime >= 1:
+while lifetime >= 1 or lifetime < 0:
  number = get_random()
  root = round(math.sqrt(number))
  prime = "yes"
