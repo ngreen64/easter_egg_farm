fa3bff854ac149060b558de894d0202254931108
ngreen64 <ng.x64.me@gmail.com>
2018-01-30 09:51:25 +0000

    Update create_eggs.py

--- code/create_eggs.py
+++ code/create_eggs.py
@@ -3,9 +3,20 @@ import random
 import math
 import sys
 import uuid
+from time import sleep
+import signal
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+    
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
 if len(sys.argv) < 2:
-    print("No arguments passed to script, generating infinite eggs between 1 and 100,000,000,000")
+    print("No arguments passed to script, generating infinite eggs between 1 and 99,999,999,999")
     lifetime = -1
     upperlimit = 99999999999
 else:
@@ -19,6 +30,7 @@ def get_random():
     return random.randint(2,upperlimit)
 
 while lifetime >= 1 or lifetime < 0:
+ killer = GracefulKiller()
  number = get_random()
  root = round(math.sqrt(number))
  prime = "yes"
@@ -32,4 +44,10 @@ while lifetime >= 1 or lifetime < 0:
  if prime == "yes":
     print(str(number))
     print(name_egg())
+    print('123')
+    sleep(1.1)
     lifetime -= 1
+    if killer.kill_now:
+      break
+
+print("Script exiting gracefully.")
